bb7092a77186eec52be7fedad9b1e9ed5dec94cc
Matehdez <mateonhernandez@outlook.com>
2025-04-01 20:22:09 -0300

    Codigo Terminado (Planteamiento problema + Tests

--- TPI1_Validador_Correlativas/TPI1_vcorrelativas/src/test/java/domain/entities/InscripcionTest.java
+++ TPI1_Validador_Correlativas/TPI1_vcorrelativas/src/test/java/domain/entities/InscripcionTest.java
@@ -1,4 +1,73 @@
 package domain.entities;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class InscripcionTest {
+    Alumno unAlumno;
+
+    Materia ayed;
+    Materia pdp;
+    Materia IySoc;
+    Materia disenio;
+
+    Inscripcion unaInscripcion;
+
+    @BeforeEach
+    public void setUp() {
+        unAlumno = new Alumno("Mateo", "Hernandez", "090.128-5");
+        ayed = new Materia("AYED");
+        pdp = new Materia("PDP");
+        IySoc = new Materia("IySoc");
+        disenio = new Materia("Diseño");
+
+        unaInscripcion = new Inscripcion(unAlumno);
+
+        pdp.agregarMateriaCorrelativa(ayed);
+        disenio.agregarMateriaCorrelativa(pdp);
+    }
+
+    @Test
+    public void testInscripcionValidaCorrelitvasIndirectas(){
+        unAlumno.agregarMateriaAprobada(ayed);
+        unAlumno.agregarMateriaAprobada(pdp);
+        unaInscripcion.addMateriaACursar(disenio);
+
+        assertTrue(unaInscripcion.aprobada());
+    }
+
+    @Test
+    public void testInscripcionValidaCorrelitvasDirectas(){
+        unAlumno.agregarMateriaAprobada(ayed);
+        unaInscripcion.addMateriaACursar(pdp);
+
+        assertTrue(unaInscripcion.aprobada());
+    }
+
+    @Test
+    public void testInscripcionInvalidaCorrelativasDirectas(){
+        unaInscripcion.addMateriaACursar(pdp);
+
+        assertFalse(unaInscripcion.aprobada());
+    }
+
+    @Test
+    public void testInscripcionValidaCorrelativasIndirectas(){
+        unAlumno.agregarMateriaAprobada(ayed);
+        unaInscripcion.addMateriaACursar(disenio);
+
+        assertFalse(unaInscripcion.aprobada());
+    }
+
+    @Test
+    public void testInscripcionValidaSinCorrelativas(){
+        unAlumno.agregarMateriaAprobada(IySoc);
+        unaInscripcion.addMateriaACursar(IySoc);
+
+        assertTrue(unaInscripcion.aprobada());
+    }
 }
+
--- TPI1_Validador_Correlativas/TPI1_vcorrelativas/src/test/java/domain/entities/MateriaTest.java
+++ TPI1_Validador_Correlativas/TPI1_vcorrelativas/src/test/java/domain/entities/MateriaTest.java
@@ -1,4 +1,23 @@
 package domain.entities;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MateriaTest {
+    Materia unaMateria;
+    Materia materiaCorrelativa;
+
+    @BeforeEach
+    public void setUp() {
+        unaMateria = new Materia("Matemáticas II");
+        materiaCorrelativa = new Materia("Matemáticas I");
+    }
+
+    @Test
+    public void testAgregarCorrelativa(){
+        unaMateria.agregarMateriaCorrelativa(materiaCorrelativa);
+        assertTrue(unaMateria.getMateriasCorrelativas().contains(materiaCorrelativa));
+    }
 }
